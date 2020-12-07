551bb91e2722358813d3971195fc95e022a11d41
stephaniemaddison <stephanieps2015@htomail.com>
2020-12-07 09:46:07 -0300

    Lombok inserido no projeto

--- src/main/java/com/example/demo/entity/RoomEntity.java
+++ src/main/java/com/example/demo/entity/RoomEntity.java
@@ -1,11 +1,19 @@
 package com.example.demo.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name="meetingroom")
 public class RoomEntity {
@@ -31,61 +39,4 @@ public class RoomEntity {
     @Column(name = "endHour", nullable = false)
     private LocalTime endHour;
 
-    public RoomEntity() {
-
-    }
-
-    public RoomEntity(Long id, String name, LocalDate date, LocalTime startHour, LocalTime endHour) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.startHour = startHour;
-        this.endHour = endHour;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public LocalTime getStartHour() {
-        return startHour;
-    }
-
-    public void setStartHour(LocalTime startHour) {
-        this.startHour = startHour;
-    }
-
-    public LocalTime getEndHour() {
-        return endHour;
-    }
-
-    public void setEndHour(LocalTime endHour) {
-        this.endHour = endHour;
-    }
-
-    @Override
-    public String toString(){
-        return "Room [id=" + id+",name=" + name + ",date ="+ date +",startHour=" +startHour +",endHour="+ endHour +"]";
-
-    }
 }
